34e832d6a34ca5b7e0d4aea848a8209f8be0cfb1
Doron Wix, SkyLabs <dorow@skylabs.co.il>
2020-02-02 15:50:20 +0200

    first check in

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+
+//Material UI components
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import ComponentList from './components/ComponentList'
+import PageLayout from './components/PageLayout'
+
 import './App.css';
 
+//Styles
+const useStyles = makeStyles({
+  toolbarTitle: {
+      flex: 1,
+  },
+});
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const classes = useStyles();
+
+return (
+    <Container maxWidth="lg">
+        <CssBaseline />
+
+        {/*Title*/}
+        <Toolbar>
+            <Typography
+                component="h2"
+                variant="h5"
+                color="inherit"
+                align="center"
+                noWrap
+                className={classes.toolbarTitle}
+            >
+               Layout Configuration
+            </Typography>
+        </Toolbar>
+
+        {/*Inputs*/}
+        <Grid  key={2} container spacing={2}>
+            <ComponentList/>
+            <PageLayout></PageLayout>
+
+        </Grid>
+
+    </Container>
+);
 }
 
 export default App;
---
+++ src/components/PageLayout.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {makeStyles} from "@material-ui/core/styles/index";
+
+//Material UI components
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+
+
+
+//Material UI Icons
+
+
+
+const useStyles = makeStyles({
+    root: {
+        margin: '0 0 20px 0',
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+    },
+    box: {
+        marginLeft: '8px',
+        display:'block',
+        width: '50%'
+    },
+    iconButton: {
+        padding: 10,
+    }
+});
+
+export default function PageLayout() {
+    const classes = useStyles();
+
+    return(
+        <React.Fragment>
+            <Grid  item xs={15} md={20} lg={25}>
+                <Paper className={classes.root}>
+                    
+
+
+                </Paper>    
+                <Paper className={classes.root}>
+   
+                </Paper>    
+            </Grid>
+        </React.Fragment>
+    )
+}
