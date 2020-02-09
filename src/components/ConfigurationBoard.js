08543e6227d52df142859a19c91dbf4f30b9a332
Doron Wix, SkyLabs <dorow@skylabs.co.il>
2020-02-09 11:23:25 +0200

    drag items to grid layout

--- src/components/ConfigurationBoard.js
+++ src/components/ConfigurationBoard.js
@@ -4,11 +4,18 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import Paper from "@material-ui/core/Paper";
 import {addComponent} from '../actions/creators'
+import GridLayout from 'react-grid-layout';
 
 
 const ConfigurationBoard = ({classes}) =>{
     const state = useSelector(state => state);
     const dispatch = useDispatch();
+
+    const layout = [
+        {i: '0', x: 0, y: 0, w: 1, h: 2, static: true},
+        {i: '1', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+        {i: '2', x: 4, y: 0, w: 1, h: 2}
+      ];
 
     
     const onDragOver = (ev) => {
@@ -25,16 +32,18 @@ const ConfigurationBoard = ({classes}) =>{
     
     return(
         <React.Fragment>
+            <Paper id="target"  onDrop={(e) => onDrop(e)} onDragOver={(e) => onDragOver(e)}>
+                    drag here
+            </Paper>
+            <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
             {state &&
                 state.component.map(cmp =>
-                <Paper className = {classes}> 
+                <Paper key={cmp.id} className = {classes}> 
                 <h1>{cmp.id}</h1>
                 </Paper>
                 )
             }
-            <Paper id="target"  onDrop={(e) => onDrop(e)} onDragOver={(e) => onDragOver(e)}>
-                    drag here
-            </Paper>
+            </GridLayout>
             </React.Fragment>
         
     )
