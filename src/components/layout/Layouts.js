09d874b467d3cded24dfbf068710d2e270c61cd7
doron wix <doron251@gmail.com>
2020-02-15 22:37:10 +0200

    resizing2

--- src/components/layout/Layouts.js
+++ src/components/layout/Layouts.js
@@ -9,16 +9,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import _ from 'lodash'
 
-import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+
 import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
+
 import MenuIcon from '@material-ui/icons/Menu';
 
 import RGL, { WidthProvider } from "react-grid-layout";
@@ -137,8 +136,6 @@ export default function Layouts() {
       return children;
     }
 
-  
-
     return (
         <div className={classes.root}>
           <CssBaseline />
@@ -185,8 +182,6 @@ export default function Layouts() {
               
               }
               {flexGrid()}
-              
-
             </Container>
           </main>
         </div>
